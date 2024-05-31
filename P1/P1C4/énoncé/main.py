3869e9cbab9f5da7f9bf6401bdc898832f84b987
Alexi2201 <160506627+Alexi2201@users.noreply.github.com>
2024-05-31 15:09:28 +0200

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,10 @@
 # Écrivez votre code ici !
+#je crée mes variable (nom, age, age maintenant).
+nom = "Leroy"
+age = 20
+#je crée une nouvelle variable age_maintenant pour ajouté 10 a l'ancienne variable age en la reprenant.
+age_maintenant = age + 10
+#j'active mes variables dans le print.
+print (f"je m'appelle {nom} et j'ai {age} ans")
+#je change la variable age pour mettre age_maintenant.
+print (f"je m'appelle {nom} et j'ai {age_maintenant} ans maintenant")
