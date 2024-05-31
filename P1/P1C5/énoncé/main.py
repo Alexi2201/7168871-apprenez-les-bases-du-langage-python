c1b4c3b97f3bc610f0f02149a580b1966f08a853
Alexi2201 <160506627+Alexi2201@users.noreply.github.com>
2024-05-31 16:33:33 +0200

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,18 @@
-# Écrivez votre code ici !
+# je crée mes variable
+nom = "alexi"
+age = 20
+taille = 1.70
+est_etudiant = True
+# je les appelle dans un print
+print(f"je m'appel {nom}j'ai {age} je mesure {taille} es ce que je suis etudiant {est_etudiant}")
+# je les appel chacun leur tour pour faire une sorte de colonne
+print(f"nom: {nom}")
+print(f"age: {age}")
+print(f"Taille: {taille}")
+print(f"est_Etudiant: {est_etudiant}")
+print ("<--------les type des variable -------->")
+# j'appel leur type de variabla
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est_etudiant: {type(est_etudiant)}")
