af6d8e1eae305cfa1f07e2344dd0ad327771cd1f
Alexi2201 <160506627+Alexi2201@users.noreply.github.com>
2024-06-03 09:44:35 +0200

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,16 @@
-# Écrivez votre code ici !
+#creation de la variable fruits
+fruits = ["pomme", "banane", "orange"]
+#afficher la variable fruits
+print(fruits)
+#inserer a la variable fruits "kiwi" a la 3 position
+fruits.insert(3 , "kiwi")
+print(fruits)
+#supprimer le 2eme element de la variable fruits "orange"
+fruits.pop(2)
+print(fruits)
+#ajouter le fruit ananas a la place du fruit de la banane dans la variable fruits
+fruits[1] = "ananas"
+print(fruits)
+#trier la variable dans l'ordre croissante "a,z"
+fruits.sort()
+print(fruits)
