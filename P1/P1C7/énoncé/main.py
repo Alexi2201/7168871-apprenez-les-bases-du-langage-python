cabfa198f12020c2b1a388b722ae2820a48a7dd6
Alexi2201 <160506627+Alexi2201@users.noreply.github.com>
2024-06-03 11:08:20 +0200

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,18 @@
-# Écrivez votre code ici !
+#creation du dictionnaire fruits.
+fruits = {
+    "pomme":"rouge",
+    "banane":"jaune",
+    "orange":"orange",}
+print(fruits)
+#on ajoute la clé "kiwi" et la valeur "vert".
+fruits["kiwi"] = "vert"
+print(fruits)
+#on met la valeur de banane dans la variable "couleur_banane.
+couleur_banane = fruits["banane"]
+print(couleur_banane)
+#on a modifier la clé pomme pour changer sa valeur "rouge" à "vert".
+fruits["pomme"] = "vert"
+print(fruits["pomme"])
+#on supprime la clé "orange"
+fruits.pop("orange")
+print(fruits)
